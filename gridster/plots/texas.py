3c940ed6cc46eca5d11569032a666567a43c5b99
Brittain Hard <brittainhard@gmail.com>
2014-10-31 15:26:52 -0500

    Added more plots.

---
+++ gridster/plots/texas.py
@@ -0,0 +1,45 @@
+from bokeh.sampledata import us_counties, unemployment
+from bokeh.plotting import *
+from bokeh.objects import HoverTool
+from collections import OrderedDict
+from bokeh import embed
+
+county_xs=[
+    us_counties.data[code]['lons'] for code in us_counties.data
+    if us_counties.data[code]['state'] == 'tx'
+]
+county_ys=[
+    us_counties.data[code]['lats'] for code in us_counties.data
+    if us_counties.data[code]['state'] == 'tx'
+]
+
+colors = ["#F1EEF6", "#D4B9DA", "#C994C7", "#DF65B0", "#DD1C77", "#980043"]
+
+county_colors = []
+for county_id in us_counties.data:
+    if us_counties.data[county_id]['state'] != 'tx':
+        continue
+    try:
+        rate = unemployment.data[county_id]
+        idx = min(int(rate/2), 5)
+        county_colors.append(colors[idx])
+    except KeyError:
+        county_colors.append("black")
+
+output_server("texas")
+
+TOOLS="pan,wheel_zoom,box_zoom,reset,hover,previewsave"
+
+figure(plot_width=300, plot_height=300)
+
+patches(county_xs, county_ys, fill_color=county_colors, fill_alpha=0.7, tools=TOOLS,
+        line_color="white", line_width=0.5, title="Unemployment 2009")
+
+hover = curplot().select(dict(type=HoverTool))
+hover.tooltips = OrderedDict([
+    ("index", "$index"),
+    ("(x,y)", "($x, $y)"),
+    ("fill color", "$color[hex, swatch]:fill_color"),
+])
+
+tag = embed.autoload_server(curplot(), cursession())
