784bf82b7ce1349613ad6333d0a4a2b579bfc2fd
Brittain Hard <brittainhard@gmail.com>
2014-11-02 20:29:15 -0600

    Functional server grid view.

--- gridster/index.py
+++ gridster/index.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template
 from utils.utils import chunks
-from plots import brewer, lorenz, rbga
+from plots import brewer, lorenz
 
 app = Flask(__name__)
 
 @app.route("/")
 def dashboard():
-	graphs = [brewer.tag, lorenz.tag, rbga.tag]
+	graphs = [brewer.tag, lorenz.tag]
 	return render_template('dashboard.html', graphs=graphs)
 
 if __name__ == '__main__':
