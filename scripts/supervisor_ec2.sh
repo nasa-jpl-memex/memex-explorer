9424237061e5a274cd279669f91d4a06c8d171f2
Christine Doig <christine.doig.cardet@gmail.com>
2014-12-15 04:09:20 -0600

    update deployment scripts

--- scripts/supervisor_ec2.sh
+++ scripts/supervisor_ec2.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-scriptdir=/mnt/ebola-tweets/MEMEX_EBOLA/memex-jpl/ebola/scripts
+scriptdir=/mnt/ebola-tweets/memex-explorer/scripts
 
 echo "Installing Supervisord dependencies"
 
 service supervisor restart
-cp $scriptdir/supervisor_ebola_space_ec2.conf /etc/supervisor/conf.d/ebola_space.conf
+cp $scriptdir/supervisor_memex_explorer_ec2.conf /etc/supervisor/conf.d/memex_explorer.conf
 supervisorctl reread
 supervisorctl update
