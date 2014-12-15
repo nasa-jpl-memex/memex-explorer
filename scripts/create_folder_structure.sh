8915c61004218db0ef3e9b07626c01c77415d1ce
Christine Doig <christine.doig.cardet@gmail.com>
2014-12-15 13:55:49 -0600

    add permissions to folders

--- scripts/create_folder_structure.sh
+++ scripts/create_folder_structure.sh
@@ -4,5 +4,10 @@ mkdir seeds
 mkdir configs
 mkdir models
 mkdir crawls
-
 mkdir image_space
+
+chmod a+rwX -R seeds
+chmod a+rwX -R models
+chmod a+rwX -R configs
+chmod a+rwX -R crawls
+chmod a+rwX -R image_space
