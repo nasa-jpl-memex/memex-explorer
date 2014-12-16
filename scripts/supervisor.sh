ee6014adaffe7935ef18d8eb0b3ece53645d2e56
Benjamin Zaitlen <quasiben@gmail.com>
2014-12-16 12:25:16 -0600

    working supervisor scripts

--- scripts/supervisor.sh
+++ scripts/supervisor.sh
@@ -5,8 +5,7 @@ echo "Installing Supervisord dependencies"
 PATH=~/anaconda/bin:$PATH
 source activate memex-explorer
 
-cp $scriptdir/supervisor_memex_explorer.conf $scriptdir/supervisord.conf
 pushd $scriptdir
 mkdir -p supervisord_child_logs
-supervisord
+supervisord -c /vagrant/scripts/supervisor_memex_explorer.conf
 popd
