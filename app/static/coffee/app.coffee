39d957169a517dde7a1891cba9a55a57ebaa5e45
Brittain Hard <brittainhard@gmail.com>
2014-12-29 13:44:47 -0600

    Non-working, saving progress.

--- app/static/coffee/app.coffee
+++ app/static/coffee/app.coffee
@@ -2,14 +2,25 @@ class CrawlForm extends Backbone.View
 
     crawler = $ '#crawler'
     data_model = $ '#data_model'
+    new_model = $ "#new_model"
+    model_options = $ '.model_options'
+    model_radio = $ ".model_radio"
  
-    initialize: ->            
-        data_model.prop "disabled", true
-
+    initialize: ->
+        model_options.prop "disabled", true
+        
     crawler.change ->
-        if crawler.val() == 'ache'
-            data_model.prop "disabled", false
+        if crawler.val() == "ache"
+            model_options.prop "disabled", false
         else
+            model_options.prop "disabled", true
+
+    model_radio.change ->
+        if model_radio.val() == "new"
+            new_model.prop "disabled", false
             data_model.prop "disabled", true
+        if model_radio.val() == "existing"
+            new_model.prop "disabled", true
+            data_model.prop "disabled", false
 
 crawl_form = new CrawlForm
--- app/static/js/app.js
+++ app/static/js/app.js
@@ -3,7 +3,7 @@ var CrawlForm, crawl_form,
   __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
 
 CrawlForm = (function(_super) {
-  var crawler, data_model;
+  var crawler, data_model, model_options, model_radio, new_model;
 
   __extends(CrawlForm, _super);
 
@@ -15,15 +15,32 @@ CrawlForm = (function(_super) {
 
   data_model = $('#data_model');
 
+  new_model = $("#new_model");
+
+  model_options = $('.model_options');
+
+  model_radio = $(".model_radio");
+
   CrawlForm.prototype.initialize = function() {
-    return data_model.prop("disabled", true);
+    return model_options.prop("disabled", true);
   };
 
   crawler.change(function() {
-    if (crawler.val() === 'ache') {
-      return data_model.prop("disabled", false);
+    if (crawler.val() === "ache") {
+      return model_options.prop("disabled", false);
     } else {
-      return data_model.prop("disabled", true);
+      return model_options.prop("disabled", true);
+    }
+  });
+
+  model_radio.change(function() {
+    if (model_radio.val() === "new") {
+      new_model.prop("disabled", false);
+      data_model.prop("disabled", true);
+    }
+    if (model_radio.val() === "existing") {
+      new_model.prop("disabled", true);
+      return data_model.prop("disabled", false);
     }
   });
 
