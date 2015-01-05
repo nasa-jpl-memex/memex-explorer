eb2a9d09568e66287e4296268324886726d6801d
Karan Dodia <dodia.karan@gmail.com>
2015-01-05 17:38:13 -0600

    fixes #185

--- app/db_api.py
+++ app/db_api.py
@@ -148,24 +148,24 @@ def db_add_crawl(project, form, seed_filename, model=None):
 
 def db_init_ache(project, crawl):
     key = project.slug + '-' + crawl.name
-    crawled_data_uri = os.path.join(CRAWLS_PATH, crawl.name, 'data/data_monitor/crawledpages.csv')
+    crawled_data_uri = os.path.join(crawl.name, 'data/data_monitor/crawledpages.csv')
     crawled_data = DataSource(name=key + '-crawledpages',
                               data_uri=crawled_data_uri,
                               project_id=project.id)
 
-    relevant_data_uri = os.path.join(CRAWLS_PATH, crawl.name, 'data/data_monitor/relevantpages.csv')
+    relevant_data_uri = os.path.join(crawl.name, 'data/data_monitor/relevantpages.csv')
     relevant_data = DataSource(name=key + '-relevantpages',
                                data_uri=relevant_data_uri,
                                project_id=project.id,
                                crawl=crawl)
 
-    frontier_data_uri = os.path.join(CRAWLS_PATH, crawl.name, 'data/data_monitor/frontierpages.csv')
+    frontier_data_uri = os.path.join(crawl.name, 'data/data_monitor/frontierpages.csv')
     frontier_data = DataSource(name=key + '-frontierpages',
                                data_uri=frontier_data_uri,
                                project_id=project.id,
                                crawl=crawl)
 
-    harvest_data_uri = os.path.join(CRAWLS_PATH, crawl.name, 'data/data_monitor/harvestinfo.csv')
+    harvest_data_uri = os.path.join(crawl.name, 'data/data_monitor/harvestinfo.csv')
     harvest_data = DataSource(name=key + '-harvestinfo',
                               data_uri=harvest_data_uri,
                               project_id=project.id,
