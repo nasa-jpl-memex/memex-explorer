bc28b847a29b2230d47fff0cae6d528f1045c0a2
Christine Doig <christine.doig.cardet@gmail.com>
2015-01-09 14:48:03 -0600

    fix crawl page image stats

--- app/views.py
+++ app/views.py
@@ -246,7 +246,7 @@ def add_crawl(project_slug):
 @app.route('/<project_slug>/crawls')
 def crawls(project_slug):
     project = get_project(project_slug)
-    image_spaces = project.image_spaces
+    image_spaces = project.image_spaces.all()
     return render_template('crawls.html', image_space=image_spaces)
 
 
