ef9cd33ad48f3d5368d7b8be2658ccaccf17ddb7
Karan Dodia <dodia.karan@gmail.com>
2015-01-26 16:33:34 -0600

    Find particular errors

--- memex_explorer/base/unit_test.py
+++ memex_explorer/base/unit_test.py
@@ -4,8 +4,11 @@ from django.test import TestCase, Client
 # App
 from base.forms import AddProjectForm
 
+# Utility functions
+def form_errors(response):
+    return response.context['form'].errors
 
-class UnitTests(TestCase):
+class TestViews(TestCase):
 
     @classmethod
     def setUpClass(cls):
@@ -13,29 +16,26 @@ class UnitTests(TestCase):
 
     def test_front_page(self):
         response = self.client.get('/', follow=True)
-        assert b'<title>Memex Explorer</title>' in response.content
+        assert 'base/index.html' in response.template_name
 
 
     def test_project_page(self):
         response = self.client.get('/add_project', follow=True)
         assert 'base/add_project.html' in response.template_name
-        assert b'<form action="" method="post">' in response.content
 
 
     def test_add_project_no_name(self):
         response = self.client.post('/add_project',
             {'description': 'cats cats cats',
              'icon': 'fa-arrows'}, follow=True)
-        assert 'base/add_project.html' in response.template_name
-        assert b'This field is required.' in response.content
+        assert 'This field is required.' in form_errors(response)['name']
 
 
     def test_add_project_no_description(self):
         response = self.client.post('/add_project',
             {'name': 'CATS!',
              'icon': 'fa-arrows'}, follow=True)
-        assert 'base/add_project.html' in response.template_name
-        assert b'This field is required.' in response.content
+        assert 'This field is required.' in form_errors(response)['description']
 
 
     def test_add_project(self):
@@ -47,7 +47,7 @@ class UnitTests(TestCase):
         assert b'CATS!' in response.content
 
 
-class TestForms():
+class TestForms(TestCase):
 
     def test_project_form(self):
         form_data = {
