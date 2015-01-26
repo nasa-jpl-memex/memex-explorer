4c9555c617b67926f718007fff4e0f283752ebd0
Karan Dodia <dodia.karan@gmail.com>
2015-01-26 15:42:30 -0600

    Remove slash from url route

--- memex_explorer/base/urls.py
+++ memex_explorer/base/urls.py
@@ -5,7 +5,7 @@ from base import views
 
 urlpatterns = patterns('',
     url(r'^$', views.Index.as_view(), name='index'),
-    url(r'^add_project/', views.AddProject.as_view(), name='add_project'),
+    url(r'^add_project', views.AddProject.as_view(), name='add_project'),
     url(r'^(?P<project_slug>\d+)/$', views.Project.as_view(), name='project'),
 )
 
