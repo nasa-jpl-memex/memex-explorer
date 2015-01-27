1d8eb1b474ccd5f664918f757bc0a129997a5f69
Karan Dodia <dodia.karan@gmail.com>
2015-01-27 12:34:13 -0600

    Namespace projects

--- memex_explorer/base/urls.py
+++ memex_explorer/base/urls.py
@@ -5,7 +5,7 @@ from base import views
 
 urlpatterns = patterns('',
     url(r'^$', views.IndexView.as_view(), name='index'),
-    url(r'^add_project', views.AddProjectView.as_view(), name='add_project'),
-    url(r'^(?P<slug>[\w-]+)/$', views.ProjectView.as_view(), name='project'),
+    url(r'^project/add_project/$', views.AddProjectView.as_view(), name='add_project'),
+    url(r'^project/(?P<slug>[\w-]+)/$', views.ProjectView.as_view(), name='project'),
 )
 
