3b6ff404ef5032c46804d8350418b97b70122aa6
Brittain Hard <brittainhard@gmail.com>
2015-01-27 13:44:22 -0600

    Added unique validator on name.

--- memex_explorer/base/models.py
+++ memex_explorer/base/models.py
@@ -5,11 +5,12 @@ from django.core.validators import RegexValidator
 
 def alphanumeric_validator():
     return RegexValidator(r'^[a-zA-Z0-9 ]+$',
-        'Only alphanumeric characters are allowed.')
+        'Only numbers, letters, and spaces are allowed.')
 
 
 class Project(models.Model):
-    name = models.CharField(max_length=64, validators=[alphanumeric_validator()])
+    name = models.CharField(max_length=64, unique=True,
+        validators=[alphanumeric_validator()])
     slug = models.SlugField(max_length=64)
     description = models.TextField()
     icon = models.CharField(max_length=64)
