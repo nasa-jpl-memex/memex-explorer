c0fd66757865a2404996166e45accf9d13cfe87d
Karan Dodia <dodia.karan@gmail.com>
2015-01-27 12:35:24 -0600

    Update unit tests: reverse url names

--- memex_explorer/base/unit_test.py
+++ memex_explorer/base/unit_test.py
@@ -18,46 +18,67 @@ class TestViews(TestCase):
 
 
     @classmethod
-    def get(view_name, *args, **kwargs):
-        return self.client.get(reverse(view_name), *args, **kwargs)
+    def get(cls, view_name, *args, **kwargs):
+        if 'slugs' in kwargs:
+            slugs = kwargs.pop('slugs')
+            return cls.client.get(
+                reverse(view_name, kwargs=slugs),
+                *args, follow=True, **kwargs)
+        else:
+            return cls.client.get(reverse(view_name),
+                *args, follow=True, **kwargs)
 
     @classmethod
-    def post(view_name, *args, **kwargs):
-        return self.client.post(reverse(view_name), *args, **kwargs)
+    def post(cls, view_name, *args, **kwargs):
+        if 'slugs' in kwargs:
+            slugs = kwargs.pop('slugs')
+            return cls.client.post(
+                reverse(view_name, kwargs=slugs),
+                *args, follow=True, **kwargs)
+        else:
+            return cls.client.post(reverse(view_name),
+                *args, follow=True, **kwargs)
 
 
     def test_front_page(self):
-        response = self.client.get('/', follow=True)
+        response = self.get('base:index')
         assert 'base/index.html' in response.template_name
 
 
     def test_project_page(self):
-        response = self.client.post(reverse('base:add_project'),
-            follow=True)
+        response = self.post('base:add_project')
         assert 'base/add_project.html' in response.template_name
 
 
     def test_add_project_no_name(self):
-        response = self.client.post(reverse('base:add_project'),
+        response = self.post('base:add_project',
             {'description': 'cats cats cats',
-             'icon': 'fa-arrows'}, follow=True)
+             'icon': 'fa-arrows'})
         assert 'This field is required.' in form_errors(response)['name']
 
 
     def test_add_project_no_description(self):
-        response = self.client.post(reverse('base:add_project'),
+        response = self.post('base:add_project',
             {'name': 'CATS!',
-             'icon': 'fa-arrows'}, follow=True)
+             'icon': 'fa-arrows'})
         assert 'This field is required.' in form_errors(response)['description']
 
 
     def test_add_project(self):
-        response = self.client.post(reverse('base:add_project'),
+        response = self.post('base:add_project',
             {'name': 'CATS!',
              'description': 'cats cats cats',
-             'icon': 'fa-arrows'}, follow=True)
+             'icon': 'fa-arrows'})
         assert 'base/index.html' in response.template_name
         assert b'CATS!' in response.content
+
+
+    def test_add_project_with_slug(self):
+        self.test_add_project()
+
+        response = self.get('base:project',
+            slugs=dict(slug="cats"))
+        assert 'base/project.html' in response.template_name
 
 
 class TestForms(TestCase):
@@ -70,3 +91,10 @@ class TestForms(TestCase):
         form = AddProjectForm(data=form_data)
         assert form.is_valid()
 
+
+    def test_project_form_no_name(self):
+        form_data = {
+            'description': 'cats cats cats',
+            'icon': 'fa-arrows'}
+        form = AddProjectForm(data=form_data)
+        assert form.is_valid() is False
