f45298bdeb6fd349a332312be117f4de0978f031
Brittain Hard <brittainhard@gmail.com>
2015-02-06 13:57:24 -0600

    Removed icon field for projects.

--- memex_explorer/base/forms.py
+++ memex_explorer/base/forms.py
@@ -6,5 +6,5 @@ from base.models import Project
 class AddProjectForm(ModelForm):
     class Meta:
         model = Project
-        fields = ['name', 'description', 'icon']
+        fields = ['name', 'description']
 
