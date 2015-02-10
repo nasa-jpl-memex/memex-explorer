d4f19b0f308a543dce1a90ed62a7474d4af8aac1
Karan Dodia <dodia.karan@gmail.com>
2015-02-10 11:08:58 -0600

    Crispy add project form

--- memex_explorer/base/static/base/js/crawl_form.js
+++ memex_explorer/base/static/base/js/crawl_form.js
@@ -1,14 +1,18 @@
 $(document).ready(function(){
-    var crawler = $("[name='crawler']");
+    var crawler = $('input[name="crawler"]');
+    var nutch = $('#id_crawler_1');
     var crawl_model = $('#id_crawl_model');
 
-    crawl_model.prop("disabled", true);
-
-    crawler.change(function(){
-        if (crawler.val() === "nutch"){
+    function on_crawler_change(){
+        if (nutch[0].checked){
             crawl_model.prop("disabled", true);
         } else {
             crawl_model.prop("disabled", false);
         }
-    });
+    }
+
+    // In case the form is reloaded with ACHE set, fire once
+    on_crawler_change();
+
+    crawler.change(on_crawler_change);
 });
--- memex_explorer/base/urls.py
+++ memex_explorer/base/urls.py
@@ -1,19 +1,34 @@
 from django.conf.urls import patterns, include, url
-
 from base import views
 
 
+project_slug = r'^projects/(?P<slug>[\w-]+)/'
+
 urlpatterns = patterns('',
-    url(r'^$', views.IndexView.as_view(), name='index'),
-    url(r'^about/$', views.AboutView.as_view(), name='about'),
+    url(r'^$', views.IndexView.as_view(), name='index'),       # Index
+    url(r'^about/$', views.AboutView.as_view(), name='about'), # About
 
-    # project/
-    url(r'^project/add_project/$', views.AddProjectView.as_view(),
-        name='add_project'),
-    url(r'^project/(?P<slug>[\w-]+)/$', views.ProjectView.as_view(),
-        name='project'),
-
-    # # Try other "_space" applications
-    url(r'^project/(?P<slug>[\w-]+)/crawl/', include('apps.crawl_space.urls',
-        namespace="crawl_space")),
+    url(r'^add_project/$', views.AddProjectView.as_view(),
+        name='add_project'),                                   # Add Project
+    url(project_slug + r'$', views.ProjectView.as_view(),
+        name='project')                                        # Project
 )
+
+
+
+# The following lines "discover" Django applications in 
+#   based on the presence of "_space" in apps.__dict__.keys()—
+#   and builds a url route to each application, namespaced appropriately.
+#   See <https://github.com/ContinuumIO/memex-explorer/issues/316>
+
+from django.conf import settings
+space_apps = [x for x in settings.INSTALLED_APPS if "_space" in x]
+
+
+inject_urls = [
+    url(project_slug, include('apps.%s.urls' % app, namespace=app))
+    for app in space_apps
+]
+
+
+urlpatterns += patterns('', *inject_urls)
