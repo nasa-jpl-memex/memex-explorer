3e2072f895935ff246bfbc3dba0b1e652f9dac37
Karan Dodia <dodia.karan@gmail.com>
2015-02-10 11:08:14 -0600

    Crispy add crawl form

--- memex_explorer/apps/crawl_space/forms.py
+++ memex_explorer/apps/crawl_space/forms.py
@@ -1,14 +1,39 @@
+from base.forms import CrispyModelForm
+
 from django.forms import ModelForm, RadioSelect, Select
-from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Fieldset, Submit
+from crispy_forms.bootstrap import InlineRadios, FormActions
 
 from apps.crawl_space.models import Crawl, CrawlModel
 
 
-class AddCrawlForm(ModelForm):
+class AddCrawlForm(CrispyModelForm):
+
     class Meta:
         model = Crawl
         fields = ['name', 'description', 'crawler', 'crawl_model', 'seeds_list']
-        widgets = {'crawler': Select, 'crawl_model': Select}
+        widgets = {'crawl_model': Select}
+
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.set_layout()
+
+
+    def set_layout(self):
+
+        self.helper.layout = Layout(
+            Fieldset(None,
+                'name',
+                'description',
+                InlineRadios(
+                    'crawler'),
+                'crawl_model',
+                'seeds_list',
+                FormActions(
+                    Submit('submit', "Submit"))
+            )
+        )
 
 
 class AddCrawlModelForm(ModelForm):
