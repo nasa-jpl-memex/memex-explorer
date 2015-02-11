03c1d8800f5a0c25a14a5b4dfe7f59373a23b0c7
Brittain Hard <brittainhard@gmail.com>
2015-02-11 15:53:37 -0600

    Moved css properties to forms.scss.

--- memex_explorer/base/static/base/js/crawl_form.js
+++ memex_explorer/base/static/base/js/crawl_form.js
@@ -7,13 +7,12 @@ $(document).ready(function(){
     var on_crawler_change = function(){
         if (nutch[0].checked){
             crawl_model.prop("disabled", true);
-            model_div.css('background-color', '#f5f5f5');
-            model_div.css('margin', '0px');
-            model_div.css('padding', '12px 8px');
-            model_div.css('border-radius', '8px');
+            model_div.addClass("input-greyed-out");
+            model_div.removeClass("input-available");
         } else {
             crawl_model.prop("disabled", false);
-            model_div.css('background-color', '#ffffff');
+            model_div.addClass("input-available");
+            model_div.removeClass("input-greyed-out");
         }
     }
 
