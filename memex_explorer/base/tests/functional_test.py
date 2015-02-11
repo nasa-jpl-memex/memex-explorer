15039412a824ddb706cbbd20bc2c21e25fb6b338
Karan Dodia <dodia.karan@gmail.com>
2015-02-11 16:39:49 -0600

    Round out add project functional test

--- memex_explorer/base/tests/functional_test.py
+++ memex_explorer/base/tests/functional_test.py
@@ -19,22 +19,34 @@ class TestProject(LiveServerTestCase):
         super().tearDownClass()
 
     def test_add_project(self):
-        self.browser.get(self.live_server_url)
-        assert self.browser.title == 'Memex Explorer'
+
+        # Names are awesome
+        ff = self.browser
+
+        ff.get(self.live_server_url)
+        assert ff.title == 'Memex Explorer'
 
         # Click on "New Project".
-        new_project = self.browser.find_element_by_link_text("New Project")
+        new_project = ff.find_element_by_link_text("New Project")
         new_project.click()
 
         # Click "Submit" on an empty form.
-        submit = self.browser.find_element_by_id('submit-id-submit')
+        submit = ff.find_element_by_id('submit-id-submit')
         submit.click()
 
+        # Verify that errors appear as expected
+        name_error = ff.find_element_by_id("error_1_id_name")
+        assert name_error.text == 'This field is required.' 
+        description_error = ff.find_element_by_id("error_1_id_description")
+        assert description_error.text == 'This field is required.' 
 
-        name_error = bb.find_element_by_id("error_1_id_name")
-        assert name_error == 'This field is required.' 
-        description_error = bb.find_element_by_id("error_1_id_description")
-        assert description_error == 'This field is required.' 
+        # Fill out the form and submit
+        name = ff.find_element_by_id("id_name")
+        name.send_keys("Test name")
+        description = ff.find_element_by_id("id_description")
+        description.send_keys("Test description")
+        submit = ff.find_element_by_id('submit-id-submit')
+        submit.click()
 
         assert False
 
