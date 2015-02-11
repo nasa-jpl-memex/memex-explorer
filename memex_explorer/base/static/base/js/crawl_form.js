436eb323007f55d65b0bb1d65d41d2a09f7a588e
Brittain Hard <brittainhard@gmail.com>
2015-02-11 15:39:46 -0600

    Removed menu drop-down button, greyed-out crawl_model.

--- memex_explorer/base/static/base/js/crawl_form.js
+++ memex_explorer/base/static/base/js/crawl_form.js
@@ -2,12 +2,18 @@ $(document).ready(function(){
     var crawler = $('input[name="crawler"]');
     var nutch = $('#id_crawler_1');
     var crawl_model = $('#id_crawl_model');
+    var model_div = $('#div_id_crawl_model');
 
-    function on_crawler_change(){
+    var on_crawler_change = function(){
         if (nutch[0].checked){
             crawl_model.prop("disabled", true);
+            model_div.css('background-color', '#f5f5f5');
+            model_div.css('margin', '0px');
+            model_div.css('padding', '12px 8px');
+            model_div.css('border-radius', '8px');
         } else {
             crawl_model.prop("disabled", false);
+            model_div.css('background-color', '#ffffff');
         }
     }
 
