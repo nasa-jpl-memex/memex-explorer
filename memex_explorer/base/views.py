1e33b464df91b79519cb53eded39da5670579747
Brittain Hard <brittainhard@gmail.com>
2015-02-12 11:00:17 -0600

    Add project now redirects to the newly created project page.

--- memex_explorer/base/views.py
+++ memex_explorer/base/views.py
@@ -2,6 +2,7 @@
 
 from django.shortcuts import render
 from django.views import generic
+from django.contrib.messages.views import SuccessMessageMixin
 
 from base.models import Project
 from base.forms import AddProjectForm
@@ -22,11 +23,18 @@ class AboutView(generic.TemplateView):
     template_name = "base/about.html"
     
 
-class AddProjectView(generic.edit.CreateView):
+class AddProjectView(generic.edit.CreateView, SuccessMessageMixin):
     model = Project
     form_class = AddProjectForm
     template_name = "base/add_project.html"
-    success_url = "/"
+    success_message = "%(name)s was saved successfully."
+
+    def get_success_url(self):
+        return self.object.get_absolute_url()
+
+    def get_success_message(self, cleaned_data):
+        return self.success_message % dict(cleaned_data,
+                                           name=self.object.name)
 
 
 class ProjectView(generic.DetailView):
