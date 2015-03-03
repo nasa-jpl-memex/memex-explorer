b5e2aed0cc70acf8c3cf43e7bac5daeb01b87845
Brittain Hard <brittainhard@gmail.com>
2015-03-03 15:22:12 -0600

    Now writing relativepages csv file to text.

--- memex_explorer/apps/crawl_space/viz/plot.py
+++ memex_explorer/apps/crawl_space/viz/plot.py
@@ -1,9 +1,13 @@
+import os
 from abc import ABCMeta, abstractmethod
+
 from bokeh.models import ColumnDataSource
 from bokeh.plotting import Document, Session
+import pandas as pd
 
 from harvest import Harvest
 from domain import Domain
+from apps.crawl_space.settings import CRAWL_PATH
 
 
 class PlotsNotReadyException(Exception):
@@ -13,10 +17,12 @@ class PlotsNotReadyException(Exception):
 class AcheDashboard(object):
 
     def __init__(self, crawl):
-        if crawl.crawler != "ache":
+        self.crawl = crawl
+        if self.crawl.crawler != "ache":
             raise ValueError("Crawl must be using the Ache crawler.")
         self.harvest = Harvest(crawl)
         self.domain = Domain(crawl)
+        self.relevant_data = self.domain.relevant_data
 
     def get_harvest_plot(self):
         try:
@@ -31,6 +37,12 @@ class AcheDashboard(object):
         except Exception:
             return [None, None]
         return [script, div]
+
+    def write_relevant_seeds(self):
+        urls = pd.read_csv(self.relevant_data, delimiter='\t', header=None,
+                         names=['url', 'timestamp'])['url']
+        return urls.to_csv(os.path.join(CRAWL_PATH, str(self.crawl.id), 'relevant_seeds.txt'),
+                           index=False)
 
     def get_plots(self):
         harvest_plot = self.get_harvest_plot()
