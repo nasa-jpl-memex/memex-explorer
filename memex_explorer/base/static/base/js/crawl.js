1b693c0ab50157e255d44d56b255aff0ddc7cedc
Karan Dodia <dodia.karan@gmail.com>
2015-03-04 18:08:59 -0600

    Clicking on restart fires start action

--- memex_explorer/base/static/base/js/crawl.js
+++ memex_explorer/base/static/base/js/crawl.js
@@ -44,6 +44,23 @@ $( document ).ready(function() {
   });
 
 
+  $('#restartButton').on('click', function() {
+
+    $( '#status' ).text( "restarting" );
+    this.disabled = true;
+
+    $.ajax({
+      type: "POST",
+      data: {"action": "start"},
+      success: function(response) {
+        console.log(response);
+        if (response.status != "error") $( '#status' ).text(response.status);
+        else console.log(response)},
+          failure: function() {
+            $( '#status' ).text( "Error (could not restart crawl)" );
+          }
+        });
+  });
 
   setInterval(function(){
     $.ajax({
