8c19189e82658a8cf3598ae41002c9cd62fd512e
Brittain Hard <brittainhard@gmail.com>
2015-03-06 09:07:26 -0600

    Added old docs from memex explorer.

--- memex_explorer/memex/wsgi.py
+++ memex_explorer/memex/wsgi.py
@@ -8,7 +8,12 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+sys.path.insert(1, '/'.join(os.path.abspath(__file__).split('/')[:-2]))
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "memex.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+
