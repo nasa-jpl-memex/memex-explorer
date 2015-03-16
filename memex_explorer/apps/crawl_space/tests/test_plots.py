b6cafa54c332755109958fc1745611d47b1739d9
Brittain Hard <brittainhard@gmail.com>
2015-03-16 16:09:34 -0500

    First two plots tests working.

--- memex_explorer/apps/crawl_space/tests/test_plots.py
+++ memex_explorer/apps/crawl_space/tests/test_plots.py
@@ -15,7 +15,7 @@ from base.models import Project, alphanumeric_validator
 from apps.crawl_space.models import Crawl
 from apps.crawl_space.viz.plot import AcheDashboard
 
-from apps.memex.test_settings import TEST_CRAWL_DATA
+from memex.test_settings import TEST_CRAWL_DATA
 
 
 class TestPlots(UnitTestSkeleton):
@@ -25,15 +25,15 @@ class TestPlots(UnitTestSkeleton):
         """Initialize a test project and crawl model,
         and save them to the test database."""
 
-        super(TestViews, cls).setUpClass()
+        super(TestPlots, cls).setUpClass()
 
         cls.test_project = Project(
-            name = "Test",
+            name = "Test Project",
             description = "Test Project Description")
         cls.test_project.save()
 
         cls.test_crawlmodel = CrawlModel(
-            name = "Test Crawl Model",
+            name = "Test Model",
             model = cls.get_model_file(),
             features = cls.get_features_file(),
             project = cls.test_project,
@@ -41,17 +41,19 @@ class TestPlots(UnitTestSkeleton):
         cls.test_crawlmodel.save()
 
         cls.test_crawl = Crawl(
-            name = "Test Crawl",
+            name = "Test Crawl Plots",
             description = "Test Crawl Description",
             crawler = "ache",
             config = "config_default",
             seeds_list = cls.get_seeds(),
-            project = cls.test_project
-            crawl_model = cls.test_crawlmodel
+            project = cls.test_project,
+            crawl_model = cls.test_crawlmodel,
         )
         cls.test_crawl.save()
         cls.dashboard = AcheDashboard(cls.test_crawl)
-
+        cls.dashboard.domain.crawled_data = os.path.join(TEST_CRAWL_DATA, "crawledpages.csv")
+        cls.dashboard.domain.relevant_data = os.path.join(TEST_CRAWL_DATA, "relevantpages.csv")
+        cls.dashboard.harvest.harvest_data = os.path.join(TEST_CRAWL_DATA, "harvestinfo.csv")
 
     @classmethod
     def tearDownClass(cls):
@@ -72,6 +74,13 @@ class TestPlots(UnitTestSkeleton):
 
         return SimpleUploadedFile('ht.seeds', bytes('This is some content.\n'), 'utf-8')
 
-    def pass(self):
-        assert 0
+    def test_domain(self):
+        domain = self.dashboard.get_domain_plot()
+        for x in domain:
+            assert x
+
+    def test_harvest(self):
+        harvest = self.dashboard.get_harvest_plot()
+        for x in harvest:
+            assert x
 
--- memex_explorer/memex/test_settings.py
+++ memex_explorer/memex/test_settings.py
@@ -4,15 +4,18 @@ Test settings for memex project.
 """
 import warnings
 import exceptions
+import os
 
 # Use default settings, overriding only those deemed necessary
 from .settings import *
+
+PROJECT_PATH = '/'.join(os.path.dirname(os.path.abspath(__file__)).split('/')[:-1])
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'test_resources')
 
 MEDIA_URL = '/test_resources/'
 
-TEST_CRAWL_DATA = '/test_resources/test_crawl_data'
+TEST_CRAWL_DATA = PROJECT_PATH + os.path.join(MEDIA_URL, "test_crawl_data")
 
 # Ignore (particular) warnings
 # ============================
