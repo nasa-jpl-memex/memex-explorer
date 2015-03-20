13e0833b4a983eba06118d0766593fdcb9d8fd4c
Brittain Hard <brittainhard@gmail.com>
2015-03-20 16:53:24 -0500

    Fixed link to solr.

--- memex_explorer/base/static/base/js/crawl.js
+++ memex_explorer/base/static/base/js/crawl.js
@@ -89,11 +89,7 @@ $( document ).ready(function() {
   }, 5000);
 
     $("#gotoSolr").on('click', function(){
-        if (window.location.port){
-            solr_url = "http://" + window.location.hostname + ":8983/solr/#"
-        }else{
-            solr_url = window.location.origin + "/solr"
-        }
+        solr_url = "http://" + window.location.hostname + ":8983/solr/#"
         window.open(solr_url, '_blank');
     });
 
