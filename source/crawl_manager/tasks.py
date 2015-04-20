9a05dc811ccb85218a508d1f990182024a6b497c
Brittain Hard <brittainhard@gmail.com>
2015-04-20 11:31:08 -0500

    Progess on saving process id.

--- source/crawl_manager/tasks.py
+++ source/crawl_manager/tasks.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 
 import subprocess
 
-from celery import shared_task
+from celery import shared_task, Task, uuid
 
 from crawl_manager.models import CeleryTask
 
@@ -10,13 +10,23 @@ from crawl_manager.models import CeleryTask
 def db_register_task(pid):
     task = CeleryTask(pid=pid)
     task.save()
+    return "success"
+
+
+def repeat(uuid):
+    print(uuid)
 
 
 @shared_task()
-def nutch(seed_dir, crawl_dir, rounds, *args, **kwargs):
-    call = ["crawl", seed_dir, crawl_dir, rounds]
+def nutch(crawl, rounds, *args, **kwargs):
+    call = ["crawl", crawl.seeds_list, crawl.get_crawl_path(), rounds]
     proc = subprocess.Popen(call, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    db_register_task(proc.pid)
+    db_register_task(proc.pid, crawl)
     stdout, stderr = proc.communicate()
     return "FINISHED"
+
+@shared_task(bind=True)
+def add(self, x, y):
+    print(self.request.id)
+    return x + y
 
