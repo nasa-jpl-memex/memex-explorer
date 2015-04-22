ef97167f28ca21775b3e1ed3d5d9644abc34160c
Brittain Hard <brittainhard@gmail.com>
2015-04-22 13:01:18 -0500

    Basic unzip task.

--- source/task_manager/file_tasks.py
+++ source/task_manager/file_tasks.py
@@ -1,2 +1,16 @@
-from celery import shared_task, Task
+import os
+import zipfile
+
+from celery import shared_task, Task, task
+
+from base.models import Project
+
+@shared_task()
+def unzip(input_file, destination=""):
+    with zipfile.ZipFile(input_file) as archive:
+        archive.extractall()
+#        for x in archive.infolist():
+#            print(x.filename)
+#            archive.extract(x, path=destination)
+    
 
--- source/task_manager/tasks.py
+++ source/task_manager/tasks.py
@@ -1,2 +1,3 @@
 from crawl_tasks import ache, nutch
+from file_tasks import unzip
 
