4d7a76c5fc7fb566791a914ab1bf0dffccc53173
Brittain Hard <brittainhard@gmail.com>
2015-04-24 12:30:35 -0500

    Reorganized models.py.

--- source/base/models.py
+++ source/base/models.py
@@ -78,83 +78,6 @@ def zipped_file_validator():
     return RegexValidator(r'.*\.(ZIP|zip)$',
         'Only compressed archive (.zip) files are allowed.')
 
-class VolumeMount(models.Model):
-    """
-    When creating this app, where to mount it in the container.
-
-    TODO: More thinking required
-    """
-    app = models.ForeignKey(App)
-    mounted_at = models.TextField(max_length=254)
-    located_at = models.TextField(max_length=254)
-
-class App(models.Model):
-    """
-    Represents information about starting an application in a container.
-    """
-    name = models.CharField(max_length=64, unique=True,
-        validators=[alphanumeric_validator()])
-    index_url = models.URLField()
-    command = models.TextField(max_length=254)
-    port_expose = models.IntegerField()
-
-class Container(models.Model):
-    NGINX_CONFIG_TEMPLATE_PATH = os.path.join(settings.BASE_DIR, 'base/deploy_templates/nginx-reverse-proxy.conf.jinja2')
-    DOCKER_COMPOSE_TEMPLATE_PATH = os.path.join(settings.BASE_DIR, 'base/deploy_templates/docker-compose.yml.jinja2')
-    NGINX_CONFIG_DESTINATION_PATH = '/etc/nginx/sites-enabled/memex-reverse-proxy.conf'
-    DOCKER_COMPOSE_DESTINATION_PATH = os.path.join(settings.BASE_DIR, 'base/docker-compose.yml')
-
-    app = models.ForeignKey(App)
-    project = models.ForeignKey(Project)
-    "What type of app should the container be running?"
-    high_port = models.IntegerField(null=True, blank=True)
-    "If the app exposes a port, what high port does it end up exposing it on?"
-    public_path_base = models.TextField(null=True, blank=True)
-    "If the app is supposed to be served to the outside world and has a base url different than /project.name/app.name, what is it?"
-    expose_publicly = models.Boolean(default=False)
-    "Should the app be exposed publicly?"
-    running = models.BooleanField(default=False)
-    "Should the container be running?"
-
-    def slug(self):
-        return "{}{}".format(self.project.name, self.app.name)
-
-    def public_urlbase(self):
-        if not self.expose_publicly:
-            return None
-        elif self.public_path_base:
-            return self.public_path_base
-        else:
-            return "{}/{}".format(self.project.name, self.app.name)
-
-    @classmethod
-    def map_public_ports(self):
-        """
-        Create a new nginx config with an entry for every container that is supposed to be running and has a public path base.
-        Then, restart nginx.
-        """
-        nginx_template = Template(open(Container.NGINX_CONFIG_TEMPLATE_PATH, 'r').read(),
-                                    trim_blocks = True, lstrip_blocks = True)
-        nginx_config = nginx_template.render(**context)
-        with open(Container.NGINX_CONFIG_DESTINATION_PATH, 'w') as f:
-            f.write(nginx_config)
-            f.flush()
-
-    @classmethod
-    def create_containers(self):
-        """
-        Create a new docker compose file with an entry for every container that is supposed to be running.
-        Then, restart nginx.
-        """
-        nginx_template = Template(open(Container.DOCKER_COMPOSE_TEMPLATE_PATH, 'r').read(),
-                                    trim_blocks = True, lstrip_blocks = True)
-        nginx_config = nginx_template.render(**context)
-        with open(Container.DOCKER_COMPOSE_DESTINATION_PATH, 'w') as f:
-            f.write(nginx_config)
-            f.flush()
-        pass
-
-
 class Project(models.Model):
     """Project model.
 
@@ -217,3 +140,80 @@ class Project(models.Model):
 
     def __unicode__(self):
         return self.name
+
+class App(models.Model):
+    """
+    Represents information about starting an application in a container.
+    """
+    name = models.CharField(max_length=64, unique=True,
+        validators=[alphanumeric_validator()])
+    index_url = models.URLField()
+    command = models.TextField(max_length=254)
+    port_expose = models.IntegerField()
+
+class VolumeMount(models.Model):
+    """
+    When creating this app, where to mount it in the container.
+
+    TODO: More thinking required
+    """
+    app = models.ForeignKey(App)
+    mounted_at = models.TextField(max_length=254)
+    located_at = models.TextField(max_length=254)
+
+class Container(models.Model):
+    NGINX_CONFIG_TEMPLATE_PATH = os.path.join(settings.BASE_DIR, 'base/deploy_templates/nginx-reverse-proxy.conf.jinja2')
+    DOCKER_COMPOSE_TEMPLATE_PATH = os.path.join(settings.BASE_DIR, 'base/deploy_templates/docker-compose.yml.jinja2')
+    NGINX_CONFIG_DESTINATION_PATH = '/etc/nginx/sites-enabled/memex-reverse-proxy.conf'
+    DOCKER_COMPOSE_DESTINATION_PATH = os.path.join(settings.BASE_DIR, 'base/docker-compose.yml')
+
+    app = models.ForeignKey(App)
+    project = models.ForeignKey(Project)
+    "What type of app should the container be running?"
+    high_port = models.IntegerField(null=True, blank=True)
+    "If the app exposes a port, what high port does it end up exposing it on?"
+    public_path_base = models.TextField(null=True, blank=True)
+    "If the app is supposed to be served to the outside world and has a base url different than /project.name/app.name, what is it?"
+    expose_publicly = models.BooleanField(default=False)
+    "Should the app be exposed publicly?"
+    running = models.BooleanField(default=False)
+    "Should the container be running?"
+
+    def slug(self):
+        return "{}{}".format(self.project.name, self.app.name)
+
+    def public_urlbase(self):
+        if not self.expose_publicly:
+            return None
+        elif self.public_path_base:
+            return self.public_path_base
+        else:
+            return "{}/{}".format(self.project.name, self.app.name)
+
+    @classmethod
+    def map_public_ports(self):
+        """
+        Create a new nginx config with an entry for every container that is supposed to be running and has a public path base.
+        Then, restart nginx.
+        """
+        nginx_template = Template(open(Container.NGINX_CONFIG_TEMPLATE_PATH, 'r').read(),
+                                    trim_blocks = True, lstrip_blocks = True)
+        nginx_config = nginx_template.render(**context)
+        with open(Container.NGINX_CONFIG_DESTINATION_PATH, 'w') as f:
+            f.write(nginx_config)
+            f.flush()
+
+    @classmethod
+    def create_containers(self):
+        """
+        Create a new docker compose file with an entry for every container that is supposed to be running.
+        Then, restart nginx.
+        """
+        nginx_template = Template(open(Container.DOCKER_COMPOSE_TEMPLATE_PATH, 'r').read(),
+                                    trim_blocks = True, lstrip_blocks = True)
+        nginx_config = nginx_template.render(**context)
+        with open(Container.DOCKER_COMPOSE_DESTINATION_PATH, 'w') as f:
+            f.write(nginx_config)
+            f.flush()
+        pass
+
