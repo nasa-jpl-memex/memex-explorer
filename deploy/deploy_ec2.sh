31c2a9515ccbc9752b84d585b1928bfaa72ec6a3
Andrew M. Farrell <amfarrell@mit.edu>
2015-04-30 15:32:40 -0500

    deploy_ec2 should push to ec2, deploy_local deploys on a local machine.

--- deploy/deploy_ec2.sh
+++ deploy/deploy_ec2.sh
@@ -1,36 +1,3 @@
-export HOSTNAME='example.com'
-export IP_ADDR='0.0.0.0'
-export ROOT_MEMEX_PORT='8000'
-export LOCAL_SETTINGS_PATH='~/memex-explorer/source/memex/local_settings.py'
-
-sudo add-apt-repository -y ppa:keithw/mosh
-sudo apt-get update -y
-sudo apt-get install nginx docker git silversearcher-ag python-software-properties mosh tig
-
-wget http://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh
-chmod +x ./Miniconda-latest-Linux-x86_64.sh
-./Miniconda-latest-Linux-x86_64.sh -b
-echo 'export PATH=/home/ubuntu/miniconda/bin:\$PATH' >> ~/.bashrc
-source ~/.bashrc
-git clone https://github.com/memex-explorer/memex-explorer/ --branch dockerize-stable
-~/miniconda/bin/conda env update --name root --file ~/memex-explorer/environment.yml
-echo "ROOT_PORT = '$ROOT_MEMEX_PORT'" >> $LOCAL_SETTINGS_PATH
-echo "IP_ADDR = '$IP_ADDR'" >> $LOCAL_SETTINGS_PATH
-echo "HOSTNAME = '$HOSTNAME'" >> $LOCAL_SETTINGS_PATH
-~/miniconda/bin/python ~/memex-explorer/source/manage.py migrate
-
-#TODO might fail here.
-~/miniconda/bin/python ~/memex-explorer/source/manage.py generate_initial_nginx  "~/memex-explorer/source/base/deploy_templates/nginx-reverse-proxy.conf.jinja2" "~/memex-explorer/deploy/initial_nginx.conf"
-
-
-sudo cp ~/memex-explorer/deploy/initial_nginx.conf /etc/nginx/sites-enabled/default
-sudo service nginx restart
-
-def install_docker(instance):
-chmod +x ~/memex-explorer/install-docker.sh
-~/memex-explorer/install-docker.sh
-sudo docker pull elasticsearch
-sudo docker pull continuumio/tika
-sudo docker pull continuumio/kibana
-
-echo 'alias dj=\"~/miniconda/bin/python ~/memex-explorer/source/manage.py\"' >> ~/.bashrc
+#!/bin/bash
+source activate memex-deploy
+GIT_BRANCH='XXX' AWS_ID='AKIAJUKVMXICU4WHXMIA' AWS_SECRET='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' python ec2-deploy.py
