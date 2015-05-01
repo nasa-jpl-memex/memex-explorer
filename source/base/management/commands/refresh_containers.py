1b2e2b2ccaada723975a841bc7a0563d0a8aa815
Andrew M. Farrell <amfarrell@mit.edu>
2015-05-01 16:26:58 -0500

    refresh containers and nginx missing model import.

--- source/base/management/commands/refresh_containers.py
+++ source/base/management/commands/refresh_containers.py
@@ -5,6 +5,7 @@ from jinja2 import Template
 from jinja2.runtime import Context
 
 from django.core.management.base import BaseCommand, CommandError
+from base.models import Container
 
 class Command(BaseCommand):
     help = 'Generate docker-compose.yml and use it to create containers'
