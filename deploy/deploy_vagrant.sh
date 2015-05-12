ccf75fccbc6ecabf45b0c90b399fc5b4ce2dbced
Aron Ahmadia <aron@ahmadia.net>
2015-05-11 22:22:58 -0400

    Split Vagrant provision into bootstrap and app
    Also, provide a helpful message when app is running

--- deploy/deploy_vagrant.sh
+++ deploy/deploy_vagrant.sh
@@ -1,6 +1,6 @@
 export HOSTNAME='example.com'
 export IP_ADDR='0.0.0.0'
-export ROOT_MEMEX_PORT='8000'
+export ROOT_MEMEX_PORT='8000' #if you change this it must be updated in Vagrantfile as well
 export LOCAL_SETTINGS_PATH='/vagrant/source/memex/local_settings.py'
 export HOME=/home/vagrant
 
@@ -11,16 +11,19 @@ sudo apt-get install -y nginx docker git silversearcher-ag python-software-prope
 wget http://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh
 chmod +x ./Miniconda-latest-Linux-x86_64.sh
 ./Miniconda-latest-Linux-x86_64.sh -b -f
-export PATH="/home/vagrant/miniconda/bin:/usr/bin:$PATH"
-echo 'export PATH="/home/vagrant/miniconda/bin:/usr/bin:$PATH"' >> /home/vagrant/.bashrc
-source /home/vagrant/.bashrc
-/home/vagrant/miniconda/bin/conda env update --name root --file /vagrant/environment.yml
-ln -s /vagrant/source/memex/settings_files/deploy_settings.py /vagrant/source/memex/settings.py
-/home/vagrant/miniconda/bin/python /vagrant/source/manage.py migrate
+/home/vagrant/miniconda/bin/conda env create --name memex --file /vagrant/environment.yml
 
-echo 'yes' | /home/vagrant/miniconda/bin/python /vagrant/source/manage.py collectstatic
-/home/vagrant/miniconda/bin/python /vagrant/source/manage.py refresh_nginx
-/home/vagrant/miniconda/bin/python /vagrant/source/manage.py create_apps_Tika_ES_Kibana
+export PATH="/home/vagrant/miniconda/envs/memex/bin:/usr/bin:$PATH"
+echo 'export PATH="/home/vagrant/miniconda/envs/memex/bin:/usr/bin:$PATH"' >> /home/vagrant/.bashrc
+source /home/vagrant/.bashrc
+export PYTHON=/home/vagrant/envs/memex/bin/python
+
+ln -s /vagrant/source/memex/settings_files/deploy_settings.py /vagrant/source/memex/settings.py
+$PYTHON /vagrant/source/manage.py migrate
+
+echo 'yes' | $PYTHON /vagrant/source/manage.py collectstatic
+$PYTHON /vagrant/source/manage.py refresh_nginx
+$PYTHON /vagrant/source/manage.py create_apps_Tika_ES_Kibana
 
 chmod +x /vagrant/deploy/install-docker.sh
 /vagrant/deploy/install-docker.sh
@@ -29,9 +32,9 @@ sudo docker pull elasticsearch
 sudo docker pull continuumio/tika
 sudo docker pull continuumio/kibana
 
-echo 'alias dj="/home/vagrant/miniconda/bin/python /vagrant/source/manage.py"' >> /home/vagrant/.bashrc
+echo 'alias dj="$PYTHON /vagrant/source/manage.py"' >> /home/vagrant/.bashrc
 
 cd /vagrant/source/
 /sbin/start-stop-daemon --chuid vagrant --background --oknodo --start --exec $(which redis-server)
 /sbin/start-stop-daemon --chuid vagrant --background --oknodo --start --exec $(which celery) -- --workdir="/vagrant/source" -A memex worker
-/home/vagrant/miniconda/bin/python ./manage.py runserver 0.0.0.0:8000
+
