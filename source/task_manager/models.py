2126cdd151b7e47fd2e1efc535ecd47c6a423cb5
Brittain Hard <brittainhard@gmail.com>
2015-05-13 11:45:24 -0500

    Working nutch call with statistics logging.

--- source/task_manager/models.py
+++ source/task_manager/models.py
@@ -3,11 +3,12 @@ from django.db import models
 from apps.crawl_space.models import Crawl
 
 
-class CeleryTask(models.Model):
+class CrawlTask(models.Model):
     
     pid = models.IntegerField(default=0)
     crawl = models.ForeignKey(Crawl)
     uuid = models.TextField()
+    returned = models.BooleanField(default=False)
 
     def __unicode__(self):
         return self.pid
