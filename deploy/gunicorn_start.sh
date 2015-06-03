4f44af7b5d2bc2354ed3996bc5702e5bdc32b5b1
Aron Ahmadia <aron@ahmadia.net>
2015-06-03 16:28:19 -0500

    Fix gunicorn settings

--- deploy/gunicorn_start.sh
+++ deploy/gunicorn_start.sh
@@ -1,36 +1,35 @@
 #!/bin/bash
- 
+
 NAME="memex_explorer"                                  # Name of the application
-DJANGODIR=/mnt/memex-explorer/memex_explorer             # Django project directory
+DJANGODIR=/vagrant/source             # Django project directory
 SOCKFILE=/tmp/gunicorn_supervisor.sock # we will communicte using this unix socket
-USER=ubuntu					# the user to run as
-GROUP=ubuntu 					   # the group to run as
+USER=vagrant
+GROUP=vagrant
 NUM_WORKERS=3                                     # how many worker processes should Gunicorn spawn
 DJANGO_SETTINGS_MODULE=memex.settings             # which settings file should Django use
 DJANGO_WSGI_MODULE=memex.wsgi                     # WSGI module name
- 
+
 echo "Starting $NAME as `whoami`"
- 
+
 # Activate the virtual environment
 cd $DJANGODIR
 export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
 export PYTHONPATH=$DJANGODIR:$PYTHONPATH
- 
+
 # Create the run directory if it doesn't exist
 RUNDIR=$(dirname $SOCKFILE)
 test -d $RUNDIR || mkdir -p $RUNDIR
- 
+
 echo $DJANGODIR
-echo /mnt/anaconda/envs/memex/bin/gunicorn ${DJANGO_WSGI_MODULE}:application 
+echo gunicorn ${DJANGO_WSGI_MODULE}:application
 echo `pwd`
 # Start your Django Unicorn
 # Programs meant to be run under supervisor should not daemonize themselves (do not use --daemon)
-/mnt/anaconda/envs/memex/bin/gunicorn ${DJANGO_WSGI_MODULE}:application \
-#  --name $NAME \
+gunicorn ${DJANGO_WSGI_MODULE}:application \
+  --name $NAME \
   --workers $NUM_WORKERS \
-  --user=$USER --group=$GROUP 
-#  --bind=unix:$SOCKFILE \
+  --user $USER --group $GROUP \
+#  --bind 0.0.0.0:8000
+  --bind unix:$SOCKFILE \
 #  --log-level=debug \
 #  --log-file=-
-
-
