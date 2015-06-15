83c8789194aad2e77cc1b3e8dca80c870df651ea
Brittain Hard <brittainhard@gmail.com>
2015-06-15 15:38:08 -0500

    Basic working ajax upload.

--- source/base/static/base/js/upload_ajax.js
+++ source/base/static/base/js/upload_ajax.js
@@ -2,19 +2,29 @@ $(document).ready(function(){
 (function(){
 
   var uploadAjax = document.getElementById("upload_data");
+  window.uploadAjax = uploadAjax;
 
   uploadAjax.onsubmit = function(event){
     event.preventDefault();
     var zipfile = uploadAjax.uploaded_data.files[0];
-    var name = uploadAjax.name;
+    var indexName = uploadAjax.name.value;
+    var csrf_token = uploadAjax.csrfmiddlewaretoken.value;
 
     var formData = new FormData();
-    formData.append("name", name);
+    formData.append("csrfmiddlewaretoken", csrf_token);
+    formData.append("name", indexName);
     formData.append("uploaded_data", zipfile, zipfile.name);
 
-    window.formData = formData;
-
     var xhr = new XMLHttpRequest();
+    xhr.open('POST', uploadAjax.action, true);
+    xhr.onload = function(){
+      if(xhr.status === 200){
+        console.log("Success");
+      }else{
+        console.log("Error");
+      }
+    }
+    xhr.send(formData);
   }
 
 })();
