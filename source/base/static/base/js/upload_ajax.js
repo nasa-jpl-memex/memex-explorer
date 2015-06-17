8d7155cbde890eb0e36861b98659ad31bb2b9007
Brittain Hard <brittainhard@gmail.com>
2015-06-17 09:39:05 -0500

    Minor design changes.

--- source/base/static/base/js/upload_ajax.js
+++ source/base/static/base/js/upload_ajax.js
@@ -2,18 +2,18 @@ $(document).ready(function(){
 (function(){
 
   var uploadAjax = document.getElementById("upload_data");
-  var progress = $("#upload_progress");
+  var progressBar = $("#progress");
+  var uploadProgress = $("#upload_progress");
   var percentage = $("#upload_percentage");
 
   function updateProgress(event){
     var percentComplete = parseInt((event.loaded / event.total) * 100);
-    progress.attr("aria-valuenow", percentComplete);
-    progress.css("width", percentComplete + "%");
+    uploadProgress.attr("aria-valuenow", percentComplete);
+    uploadProgress.css("width", percentComplete + "%");
     percentage.html(percentComplete + "%");
   }
 
   var xhr = new XMLHttpRequest();
-
   xhr.upload.addEventListener("progress", updateProgress, false);
   xhr.open('POST', uploadAjax.action, true);
   xhr.onload = function(){
@@ -26,6 +26,7 @@ $(document).ready(function(){
 
   uploadAjax.onsubmit = function(event){
     event.preventDefault();
+    progressBar.attr("hidden", false);
     //window.open('http://www.google.com/', 'newwindow', config='height=600, width=600, toolbar=no, menubar=no, scrollbars=no, resizable=no, location=no, directories=no, status=no');
     var zipfile = uploadAjax.uploaded_data.files[0];
     var indexName = uploadAjax.name.value;
