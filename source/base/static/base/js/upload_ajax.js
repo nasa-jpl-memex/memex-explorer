2a771692a6a1c87e404fbf0d6b91e589ec277779
Brittain Hard <brittainhard@gmail.com>
2015-06-17 13:56:29 -0500

    Form now disabled when upload is in progress.

--- source/base/static/base/js/upload_ajax.js
+++ source/base/static/base/js/upload_ajax.js
@@ -14,6 +14,7 @@ $(document).ready(function(){
   }
 
   var success = false;
+  var uploading = false;
 
   var xhr = new XMLHttpRequest();
   xhr.upload.addEventListener("progress", updateProgress, false);
@@ -21,12 +22,24 @@ $(document).ready(function(){
   xhr.onload = function(){
     if (xhr.status === 200){
       success = true;
+      uploading = false;
     } else {
       success = false;
+      uploading = false;
     }
   }
 
+  function disableForm(formId){
+    $("#" + formId).find(":input").attr("disabled", true);
+    $("#" + formId).css({
+      "background-color": "#f5f5f5",
+      "border-radius": "8px",
+    });
+  }
+
   uploadAjax.onsubmit = function(event){
+    uploading = true;
+    disableForm("upload_data");
     event.preventDefault();
     progressBar.attr("hidden", false);
     //window.open('http://www.google.com/', 'newwindow', config='height=600, width=600, toolbar=no, menubar=no, scrollbars=no, resizable=no, location=no, directories=no, status=no');
@@ -43,7 +56,7 @@ $(document).ready(function(){
   }
 
   window.onbeforeunload = function(){
-    if (success != true){
+    if (uploading == true){
       return "Reloading or navgating away from this page will cause the upload to fail.";
     }
   }
