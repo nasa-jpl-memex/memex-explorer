fbe80a71736c04999bc837b78028a21720438505
Brittain Hard <brittainhard@gmail.com>
2015-07-13 12:30:39 -0500

    Almost-working elasticsearch recipie.

---
+++ conda_recipies/elasticsearch/build.sh
@@ -0,0 +1,18 @@
+mkdir -vp ${PREFIX}/bin;
+mkdir -vp ${PREFIX}/lib/elasticsearch/bin;
+mkdir -vp ${PREFIX}/lib/elasticsearch/lib;
+mkdir -vp ${PREFIX}/lib/elasticsearch/config;
+mkdir ${PREFIX}/lib/elasticsearch/lib/sigar;
+
+mvn clean package -DskipTests;
+
+cp -r target/lib/* ${PREFIX}/lib/elasticsearch/lib/;
+cp target/elasticsearch-1.6.0.jar ${PREFIX}/lib/elasticsearch/lib/elasticsearch-1.6.0.jar;
+cp -r target/bin/* ${PREFIX}/lib/elasticsearch/bin/;
+cp -r config/* ${PREFIX}/lib/elasticsearch/config/;
+cp -r lib/sigar/* ${PREFIX}/lib/elasticsearch/lib/sigar/;
+cp ${RECIPE_DIR}/elasticsearch.yml ${PREFIX}/lib/elasticsearch/config/elasticsearch.yml;
+
+rm ${PREFIX}/lib/elasticsearch/bin/*.exe;
+
+chmod +x ${PREFIX}/lib/elasticsearch/bin/elasticsearch;
