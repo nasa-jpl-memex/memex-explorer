73e7c0698dc72e3bf2cbe0b7a87ae5d7d79f45cd
Brittain Hard <brittainhard@gmail.com>
2015-07-17 10:36:20 -0500

    Updated logio creation script.

--- deploy/logio_settings.py
+++ deploy/logio_settings.py
@@ -2,13 +2,14 @@ import jinja2
 import os
 
 conda_env_location = os.environ["CONDA_ENV_PATH"]
+conda_env_name = os.environ["CONDA_DEFAULT_ENV"]
 
 p = jinja2.Template(
 """exports.config = {
   nodeName: "application_server",
   logStreams: {
     nutch: [
-      "{{ conda_env_location }}/lib/nutch/logs/hadoop.log"
+      "{{ conda_env_location }}/envs/{{ conda_env_name }}/lib/nutch/logs/hadoop.log"
     ]
   },
   server: {
@@ -19,5 +20,5 @@ p = jinja2.Template(
 )
 
 f = open(os.path.join(os.environ["HOME"], ".log.io", "harvester.conf"), "w")
-f.write(p.render(conda_env_location=conda_env_location))
+f.write(p.render(conda_env_location=conda_env_location, conda_env_name=conda_env_name))
 f.close()
