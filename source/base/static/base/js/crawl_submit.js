4c489e9a4bb7ac70c5c9fa5d7818e712ae4c1b18
Brittain Hard <brittainhard@gmail.com>
2015-07-22 16:13:38 -0400

    Iteration on adding crawl.

--- source/base/static/base/js/crawl_submit.js
+++ source/base/static/base/js/crawl_submit.js
@@ -1,24 +1,23 @@
 (function(){
   $(document).ready(function(){
 
-    var xhr = new XMLHttpRequest();
-
-    xhr.open('POST', window.location.href + "add_crawl/", true);
-    xhr.onload = function(){
-      if ((xhr.status === 200) || (xhr.status === 302)){
-        success = true;
-      } else {
-        success = false;
-      }
-    }
-
     var addCrawlForm = document.getElementById("addCrawlForm")
 
     addCrawlForm.onsubmit = function(event){
       event.preventDefault();
-      var formData = new FormData();
 
-      var seedsList = addCrawlForm.seeds_list.files[0];
+      var xhr = new XMLHttpRequest();
+
+      xhr.open('POST', window.location.href + "add_crawl/", true);
+      xhr.onload = function(){
+        if ((xhr.status === 200) || (xhr.status === 302)){
+          success = true;
+        } else {
+          success = false;
+        }
+      }
+
+      var seeds_list = addCrawlForm.seeds_list.files[0];
       var name = addCrawlForm.name.value;
       var csrfmiddlewaretoken = addCrawlForm.csrfmiddlewaretoken.value;
       var description = addCrawlForm.description.value;
@@ -26,13 +25,15 @@
       var rounds_left = addCrawlForm.rounds_left.value;
       var textseeds = addCrawlForm.textseeds.value;
 
-      //formData.append("seeds_list", seedsList, seedsList.name)
-      formData.append("name", name)
-      formData.append("csrfmiddlewaretoken", csrfmiddlewaretoken)
-      formData.append("description", description)
-      formData.append("crawl_model", crawl_model)
-      formData.append("rounds_left", rounds_left)
-      formData.append("textseeds", textseeds)
+      var formData = new FormData();
+
+      formData.append("csrfmiddlewaretoken", csrfmiddlewaretoken);
+      formData.append("name", name);
+      formData.append("description", description);
+      formData.append("crawl_model", crawl_model);
+      formData.append("rounds_left", rounds_left);
+      formData.append("textseeds", textseeds);
+      formData.append("seeds_list", seeds_list, seeds_list.name);
 
       xhr.send(formData);
     }
