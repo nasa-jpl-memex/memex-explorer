e14c9ea16db96e1053f0e2cc8b8a7a3a15a6cb25
Brittain Hard <brittainhard@gmail.com>
2015-07-24 13:39:28 -0400

    Fixed app_update script.

--- source/app_setup.sh
+++ source/app_setup.sh
@@ -5,16 +5,16 @@ conda install conda-env -y;
 conda env update --file local-environment.yml;
 popd;
 
+pushd memex;
+cp settings_files/dev_settings.py settings.py;
+popd;
+
 source activate memex;
 npm install -g log.io
 python manage.py migrate;
 
 pushd ../deploy;
 python logio_settings.py;
-popd;
-
-pushd memex;
-cp settings_files/dev_settings.py settings.py;
 popd;
 
 source deactivate;
