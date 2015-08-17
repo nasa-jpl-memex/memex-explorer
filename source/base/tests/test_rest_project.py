0ed736d017ea64ee074bff71e8090565217b46d6
Brittain Hard <brittainhard@gmail.com>
2015-08-17 17:04:19 -0500

    Added tests for changing name and description.

--- source/base/tests/test_rest_project.py
+++ source/base/tests/test_rest_project.py
@@ -29,36 +29,47 @@ class TestRestProject(APITestCase):
             description = "Testing Rest API"
         )
         cls.test_project.save()
+        cls.url = "/api/projects/"
 
     def test_add_project(self):
-        response = self.client.post("/api/projects/", {"name":"Postresttest"}, format="json")
+        response = self.client.post(self.url, {"name":"Postresttest"}, format="json")
         assert response.data["slug"] == "postresttest"
         assert response.data["name"] == "Postresttest"
 
     def test_add_project_no_name(self):
-        response = self.client.post("/api/projects/", {}, format="json")
+        response = self.client.post(self.url, {}, format="json")
         assert response.data["name"][0] == "This field is required."
 
     def test_add_project_no_name(self):
-        response = self.client.post("/api/projects/", {"name":"potatorest!"}, format="json")
+        response = self.client.post(self.url, {"name":"postrest!"}, format="json")
         assert response.data["name"][0] == "Only numbers, letters, underscores, dashes and spaces are allowed."
 
     def test_get_all_projects(self):
-        response = self.client.get("/api/projects/")
+        response = self.client.get(self.url)
         assert response.data
 
     def test_get_project_by_id(self):
-        response = self.client.get("/api/projects/?id=%s" % self.test_project.id)
+        response = self.client.get(self.url + "?id=%s" % self.test_project.id)
         assert response.data
 
     def test_get_project_by_name(self):
-        response = self.client.get("/api/projects/?name=%s" % self.test_project.name)
+        response = self.client.get(self.url + "?name=%s" % self.test_project.name)
         assert response.data
 
     def test_get_project_by_slug(self):
-        response = self.client.get("/api/projects/?slug=%s" % self.test_project.slug)
+        response = self.client.get(self.url + "?slug=%s" % self.test_project.slug)
         assert response.data
 
     def test_no_project_exists(self):
-        response = self.client.get("/api/projects/?id=115")
+        response = self.client.get(self.url + "?id=115")
         assert not response.data
+
+    def test_change_name(self):
+        response = self.client.patch(self.url + "%d/" % self.test_project.id,
+            {'name':'newname'}, format="json")
+        assert response.data["name"] == "newname"
+
+    def test_change_description(self):
+        response = self.client.patch(self.url + "%d/" % self.test_project.id,
+            {'description':'New Description'}, format="json")
+        assert response.data["description"] == "New Description"
