2dc8018b9f604b19decae6d387bdf33e3a6831fa
Brittain Hard <brittainhard@gmail.com>
2015-10-01 12:51:53 -0500

    Add seeds view and form.

--- source/base/static/base/js/backbone/seeds.js
+++ source/base/static/base/js/backbone/seeds.js
@@ -36,6 +36,52 @@
   });
 
 
+  var AddSeedsView = BaseViews.FormView.extend({
+    el: "#addSeedsContainer",
+    modal: "#newSeedsModal",
+    form: "#addSeedsForm",
+    formFields: ["name"],
+    template: _.template($("#addSeedsTemplate").html()),
+    initialize: function(collection){
+      this.collection = collection;
+      this.render();
+    },
+    render: function(){
+      this.$el.html(this.template());
+    },
+    addSeeds: function(event){
+      var that = this;
+      event.preventDefault();
+      var formObjects = this.toFormData(this.form);
+      // Attach the contents of the file to the FormData object.
+      var file = $(this.filesField)[0].files[0];
+      if (typeof file != 'undefined'){
+        formObjects.append("seeds_list", file, file.name);
+      }
+      var newSeeds = new Seeds(formObjects);
+      this.collection.add(newSeeds);
+      // If model.save() is successful, clear the errors and the form, and hide
+      // the modal. If model.save() had errors, show each error on form field,
+      // along with the content of the error.
+      newSeeds.save({}, {
+        success: function(response){
+          var newSeeds = new SeedsView(
+            that.collection.models[that.collection.models.length - 1]
+          );
+          that.formSuccess(that.modal, that.form);
+          that.clearErrors(that.formFields, that.form);
+        },
+        error: function(model, xhr, thrownError){
+          that.showFormErrors(xhr.responseJSON, that.form);
+        },
+      });
+    },
+    events: {
+      "submit #addSeedsForm": "addSeeds",
+    },
+  });
+
+
   var SeedsRouter = Backbone.Router.extend({
     routes: {
       "": "index",
@@ -43,7 +89,7 @@
     index: function(){
       var seedsCollection = new SeedsCollection();
       var seedsCollectionView = new SeedsCollectionView(seedsCollection);
-      // var addProjectView = new AddProjectView(projectCollection);
+      var addSeedsView = new AddSeedsView(seedsCollection);
     },
   });
 
