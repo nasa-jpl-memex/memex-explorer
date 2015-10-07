1d12cdaf98f3ee4fb6f7fcbc023a9534a96ab127
Brittain Hard <brittainhard@gmail.com>
2015-10-07 15:50:39 -0500

    App properly fetches seeds list.

--- source/base/static/base/js/backbone/edit_seeds_backbone.js
+++ source/base/static/base/js/backbone/edit_seeds_backbone.js
@@ -1,10 +1,38 @@
 (function(exports){
 
-  exports.SeedsView = Backbone.View.extend({
+  var EditSeedsView = Backbone.View.extend({
     el: "#seeds",
+    template: _.template($("#editSeeds").html()),
+    initialize: function(model){
+      this.model = model;
+      var that = this;
+      this.model.fetch({
+        success: function(){
+          that.render();
+        }
+      });
+    },
+    render: function(){
+      this.$el.append(this.template(this.model.toJSON()));
+    }
   });
 
 
-  exports.EditSeedsRouter = Backbone.Router.extend({});
+  var EditSeedsRouter = Backbone.Router.extend({
+    routes: {
+      "": "index",
+    },
+    index: function(){
+      var seedsList = new Seeds.Seeds();
+      var seedsListPk = $("#seeds_pk").val();
+      seedsList.urlRoot = seedsList.urlRoot + seedsListPk + "/";
+      var seedsView = new EditSeedsView(seedsList);
+    },
+  });
+
+  $(document).ready(function(){
+    var appRouter = new EditSeedsRouter();
+    Backbone.history.start();
+  });
 
 })(this.EditSeeds = {});
